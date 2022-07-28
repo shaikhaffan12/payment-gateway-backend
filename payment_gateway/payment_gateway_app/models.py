1f4ce30a9c1e410ee1c17852b5f942d5cb9c6957
shaikhaffan12 <shaikh.affan@mindbowser.com>
2022-07-28 23:31:48 +0530

    implmented web hooks

--- payment_gateway/payment_gateway_app/models.py
+++ payment_gateway/payment_gateway_app/models.py
@@ -1,8 +1,9 @@
+import email
 from unicodedata import name
 from django.db import models
 
 # Create your models here.
-class product(models.Model):
+class product(models.Model): # models for product 
     name = models.CharField(max_length=255, blank=False)
     description  = models.TextField(max_length=2555, blank=False)
     price = models.IntegerField(blank=False)
@@ -10,3 +11,14 @@ class product(models.Model):
     def __str__(self):
         return self.name + ' ' + str(self.price)
 
+class payment_detail(models.Model): # model for payment details 
+    email = models.EmailField(blank=False)
+    name = models.CharField(max_length=255, blank=False)
+    amount = models.IntegerField(blank=False)
+    city = models.CharField(max_length=255, blank=False)
+    state = models.CharField(max_length=255, blank=False)
+    country = models.CharField(max_length=255, blank=False)
+    status = models.CharField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.name + ' || ' + self.email
--- payment_gateway/payment_gateway_app/serializer.py
+++ payment_gateway/payment_gateway_app/serializer.py
@@ -2,7 +2,7 @@ from pyexpat import model
 from rest_framework import serializers
 from .models import product
 
-class productSerializer(serializers.ModelSerializer):
+class productSerializer(serializers.ModelSerializer):  # product detail serializer
     class Meta:
         model = product
         fields = '__all__'
